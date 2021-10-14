a8c6c1f53189422e3bdf104283817e966fe6606b
erasromani <ekr6072@nyu.edu>
2021-10-14 02:37:12 -0400

    added ngram and rnn model

--- pytorch_wrapper/models/nlp_models.py
+++ pytorch_wrapper/models/nlp_models.py
@@ -1,8 +1,189 @@
 import gin
+import numpy as np
 import torch
-import torch.nn as nn
+from torch import nn
 import torch.nn.functional as F
 from attrdict import AttrDict
+
+
+def sample_from_distribution(dist):
+    tokens, p = zip(*dist.items())
+    return np.random.choice(tokens, p=p)
+
+
+class Node:
+    def __init__(self):
+        self.count = 0
+        self.child_nodes = {}
+
+    def has_token(self, token):
+        return token in self.child_nodes
+
+    def create_child_node(self, token):
+        self.child_nodes[token] = Node()
+
+    def increment_count(self):
+        self.count += 1
+    
+    def get_child_node(self, token):
+        if token not in self.child_nodes:
+            raise KeyError(f"'{token}' not found in child node")
+        return self.child_nodes[token]
+        
+
+class NgramTrie:
+    def __init__(self, n):
+        self.n = n
+        self.root_node = Node()
+        self.vocab = set()
+    
+    def add_ngram(self, ngram):
+        if len(ngram) > self.n:
+            raise RuntimeError(f"This NgramTrie only supports up to {n}-grams")
+
+        # Update vocabulary
+        self.vocab.update(ngram)
+
+        # Update Trie
+        current_node = self.root_node
+        current_node.increment_count()
+        for token in ngram:
+            if not current_node.has_token(token):
+                current_node.create_child_node(token)
+            current_node = current_node.get_child_node(token)
+            current_node.increment_count()
+        
+    def fit(self, lines):
+        for line in lines:
+            sequence = ["<bos>"] + line.split() + ["<eos>"]
+            for i in range(len(sequence)-self.n+1):
+                # for each ngram (sliding window)
+                ngram = tuple(sequence[i:i+self.n])
+                self.add_ngram(ngram)
+    
+    def get_ngram_cond_dist(self, input_ngram, variant=None):
+        """Get n-gram conditional distribution from trie.
+
+        Arguments:
+        input_ngram: Tuple of n-1 tokens
+
+        Returns:
+        Dictionary mapping each possible token->conditional probability
+        """
+        current_node = self.root_node
+        if variant == "backoff":
+            # Repeatedly back-off to smaller N
+            while True:
+                current_node = self.root_node
+                matched = True
+                for token in input_ngram:
+                    # Did not find token: reduce n-gram
+                    if not current_node.has_token(token):
+                        input_ngram = input_ngram[1:]
+                        matched = False
+                        break
+                    current_node = current_node.get_child_node(token)
+
+                # Successfully matched n-gram, break out of loop
+                if matched:
+                    break
+
+            if len(input_ngram) != self.n:
+                print(f"Fell back to {len(input_ngram)+1}-gram")
+        elif variant == "smoothing":
+                try:
+                    # Traverse trie
+                    for token in input_ngram:
+                        current_node = current_node.get_child_node(token)
+                    # Get count for each child node
+                    child_counts = {}
+                    for token, node in current_node.child_nodes.items():
+                        child_counts[token] = node.count
+                except KeyError:
+                    # Unseen N-gram
+                    child_counts = {}
+        elif variant is None:
+            for token in input_ngram:
+                current_node = current_node.get_child_node(token)
+        else:
+            raise ValueError("invalid variant {} entered".format(variant))
+
+        # Get count for each child node
+        if variant == "smoothing":
+            for token in self.vocab:
+                if token not in child_counts:
+                    child_counts[token] = 0
+                child_counts[token] += 1
+        else:
+            child_counts = {}
+            for token, node in current_node.child_nodes.items():
+                child_counts[token] = node.count
+
+        # Divide by total counts
+        total_count = sum(child_counts.values())
+        child_prob = {}
+        for token, count in child_counts.items():
+            child_prob[token] = count / total_count
+        return child_prob
+
+    def sample_from_trie(self, seed_tokens, cond_dist_func, max_length=20):
+        all_tokens = seed_tokens.copy()
+        for i in range(max_length):
+            input_tokens = all_tokens[-(self.n-1):]
+            dist = cond_dist_func(input_tokens)
+            new_token = sample_from_distribution(dist)
+            all_tokens.append(new_token)
+            if new_token == "<eos>":
+                break
+        return all_tokens
+
+
+@gin.configurable
+class NgramModel(nn.Module):
+    def __init__(self, vocab_size, ngram_n, emb_dim):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.in_embed = nn.Embedding(vocab_size, emb_dim)
+        self.linear = nn.Linear(emb_dim * (ngram_n - 1), emb_dim)
+        self.out_embed = nn.Linear(emb_dim, vocab_size)
+
+        # Share weights between embeddings
+        self.out_embed.weight = self.in_embed.weight
+    
+    def forward(self, x):
+        batch_size, seq_len = x.shape
+        out = self.in_embed(x)
+        out = out.reshape(batch_size, seq_len * self.emb_dim)
+        out = F.relu(self.linear(out))
+        out = self.out_embed(out)
+        return out
+
+
+@gin.configurable
+class RNNModel(nn.Module):
+    def __init__(self, vocab_size, emb_dim, num_layers, dropout_p=0.5):
+        super().__init__()
+        self.emb_dim = emb_dim
+        self.in_embed = nn.Embedding(vocab_size, emb_dim)
+        self.rnn = nn.RNN(
+            input_size=emb_dim,
+            hidden_size=emb_dim,
+            num_layers=num_layers,
+            nonlinearity="tanh",
+            batch_first=True,
+        )
+        self.dropout = nn.Dropout(dropout_p)
+        self.out_embed = nn.Linear(emb_dim, vocab_size)
+
+        # Share weights between embeddings
+        self.out_embed.weight = self.in_embed.weight
+    
+    def forward(self, x):
+        out = self.in_embed(x)
+        # h_0 is all zeros if it is not provided
+        out, hidden = self.rnn(out)
+        out = self.out_embed(self.dropout(out))
+        return out
 
 
 @gin.configurable
